06d3919fdee7801a53fa8586ded2a3fb5219a8fa
AuroraPolaire <sea.forest.mountains@gmail.com>
2022-12-25 13:58:44 +0200

    2

---
+++ scripts/scripts.js
@@ -0,0 +1,26 @@
+// function SeriesSum(n) {
+//   let sum = 1;
+//   let difference = 1;
+
+//   if (n === 1) return n.toFixed(2);
+//   if (n === 0) return n.toFixed(2);
+
+//   for (let i = 2; i <= n; i += 1) {
+//     sum += 1 / (difference += 3);
+//     console.log(sum);
+//   }
+//   return sum.toFixed(2);
+// }
+
+// console.log(SeriesSum(4));
+
+
+function friend(friends){
+  let friendsArray = [];
+  for ( const friend of friends) {
+    if (typeOf friend === 'string' && friend.length <= 4) {friendsArray.push(friend)}
+      }
+  return friendsArray
+}
+
+console.log(friend(["Ryan", "Jimmy", "123", "4", "Cool Man"]));
